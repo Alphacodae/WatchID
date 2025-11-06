5b6cfe241c8060121c34c3cd6641c6dbcf023f2a
Gyanam Shah <gyanamshah18@gmail.com>
2025-11-06 09:37:43 +0530

    Third Commit V0.3

--- age_classifier/server.py
+++ age_classifier/server.py
@@ -1,9 +1,19 @@
 from fastapi import FastAPI, UploadFile, File
+from fastapi.middleware.cors import CORSMiddleware
 import shutil
 import os
 from run_model import predict_age_api
 
 app = FastAPI()
+
+# Enable CORS for frontend access
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173", "http://localhost:3000"],  # Vite dev server
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.post("/predict-age/")
 async def predict_age_endpoint(file: UploadFile = File(...)):
